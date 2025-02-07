3b38fc04c9b861ce63041b878c06875a3c690d67
Anthony Ashcraft <anthonyashcraft91@gmail.com>
2025-02-07 02:40:24 -0500

    fixing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,16 @@ export default function App() {
   const [allCourses, setAllCourses] = useState([]);
   const [singleCourse, setSingleCourse] = useState([]);
 
+  const [newCourse, setNewCourse] = useState({
+    englishTitle: "",
+    spanishTitle: "",
+    englishLink: "",
+    spanishLink: "",
+    category: "",
+    contentType: "",
+    active: true
+  });
+
   const getCourses = async () => {
     try {
       const res = await fetch("/api/courses");
@@ -33,12 +43,113 @@ export default function App() {
     }
   };
 
+  const createCourse = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("/api/courses", {
+        method: "POST",
+        body: JSON.stringify(newCourse),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
+      if (!res.ok) {
+        const notify = () => toast("Error");
+        notify();
+      } else {
+        const notify = () => toast(newCourse?.englishTitle);
+        notify();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getCourses();
   }, []);
 
   return (
     <>
+      <form onSubmit={createCourse}>
+        <input
+          placeholder="English Title"
+          name="englishTitle"
+          value={newCourse.englishTitle}
+          onChange={(e) => {
+            setNewCourse({ ...newCourse, englishTitle: e.target.value });
+          }}
+        />
+
+        <input
+          placeholder="Spanish Title"
+          name="spanishTitle"
+          value={newCourse.spanishTitle}
+          onChange={(e) => {
+            setNewCourse({ ...newCourse, spanishTitle: e.target.value });
+          }}
+        />
+
+        <input
+          placeholder="English Link"
+          name="englishLink"
+          value={newCourse.englishLink}
+          onChange={(e) => {
+            setNewCourse({ ...newCourse, englishLink: e.target.value });
+          }}
+        />
+
+        <input
+          placeholder="Spanish Link"
+          name="spanishLink"
+          value={newCourse.spanishLink}
+          onChange={(e) => {
+            setNewCourse({ ...newCourse, spanishLink: e.target.value });
+          }}
+        />
+
+        <select
+          name="category"
+          onChange={(e) => {
+            setNewCourse({ ...newCourse, category: e.target.value });
+          }}
+        >
+          <option></option>
+          <option>Health & Wellness</option>
+          <option>Workforce Integration</option>
+        </select>
+
+        <select
+          name="contentType"
+          onChange={(e) => {
+            setNewCourse({ ...newCourse, contentType: e.target.value });
+          }}
+        >
+          <option></option>
+          <option>Video</option>
+          <option>PDF</option>
+        </select>
+
+        <input
+          type="radio"
+          name="active"
+          onClick={(e) => {
+            setNewCourse({ ...newCourse, active: true });
+          }}
+        />
+        <input
+          type="radio"
+          name="active"
+          onClick={(e) => {
+            setNewCourse({ ...newCourse, active: false });
+          }}
+        />
+
+        <button type="submit">Create Course</button>
+      </form>
+
       <Toaster
         toastOptions={{
           className: "toast"
